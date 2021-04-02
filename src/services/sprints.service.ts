9739b273f1e42eca8f274a706c13dc32e2f579a6
Alberto <agonzalez@tribalyte.com>
2021-04-02 02:51:08 +0200

    Implement mongoose function to get sprints in queries
    - Use correct mongoose functions to the a sprint and sprints in the corresponding queries

--- src/services/sprints.service.ts
+++ src/services/sprints.service.ts
@@ -1,20 +1,16 @@
 import { Service } from "typedi";
 
-import { Sprint } from "../graphql/entities/Sprint";
+import { Sprint, SprintModel } from "../graphql/entities/Sprint";
 import { GetSprintInput } from "../graphql/input/GetSprintInput";
 import { ProjectIdInput } from "../graphql/input/ProjectIdInput";
 
 @Service()
 export class SprintsService {
     public async getSprints({ projectId }: ProjectIdInput): Promise<Array<Sprint>> {
-        console.log("Getting sprints from project: ", projectId);
-        // TODO: Get sprints from database
-        return new Array<Sprint>();
+        return await SprintModel.find({ projectId });
     }
 
     public async getSprint({ projectId, sprintVersion }: GetSprintInput): Promise<Sprint | null> {
-        console.log(`Getting sprint version ${sprintVersion} from project ${projectId}`);
-        // TODO: Implement get sprint by project logic
-        return null;
+        return await SprintModel.findOne({ projectId, version: sprintVersion });
     }
 }
