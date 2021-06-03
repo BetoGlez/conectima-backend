b71f91489ce0252d773fc048200c48a917ac78cc
Alberto <agonzalez@tribalyte.com>
2021-06-03 21:09:51 +0200

    Add minor fix to resolve \n char escaped
    - Add minor fix to resolve \n char escaped from env variables

--- src/api.constants.ts
+++ src/api.constants.ts
@@ -6,7 +6,7 @@ export class ApiConstants {
         type: process.env.SHEET_TYPE || "",
         project_id: process.env.SHEET_PROJECT_ID || "",
         private_key_id: process.env.SHEET_PRIVATE_KEY_ID || "",
-        private_key: process.env.SHEET_PRIVATE_KEY || "",
+        private_key: process.env.SHEET_PRIVATE_KEY?.replace(/\\n/g, "\n") || "",
         client_email: process.env.SHEET_CLIENT_EMAIL || "",
         client_id: process.env.CLIENT_ID || "",
         auth_uri: process.env.SHEET_AUTH_URI || "",
